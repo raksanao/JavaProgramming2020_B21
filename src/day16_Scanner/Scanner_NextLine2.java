c22e44eda0342c6f8ce636c65fa0120bd25a7562
saimCybertek <saim@cybertekschool.com>
2020-10-15 09:52:38 -0500

    Day 15 Scanner

--- src/day16_Scanner/Scanner_NextLine2.java
+++ src/day16_Scanner/Scanner_NextLine2.java
@@ -8,9 +8,19 @@ public class Scanner_NextLine2 {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter your age");
+        int age = input.nextInt();
 
+        System.out.println("Enter your phone number");
+        long phoneNum = input.nextLong();
 
+        input.nextLine(); // The purpose of this is to accept the enter key
 
+        System.out.println("Enter your full name");
+        String fullName = input.nextLine();
+
+        System.out.println("Name: " + fullName);
+        System.out.println("Age: " + age);
+        System.out.println("Phone Number: " + phoneNum);
 
     }
 }
