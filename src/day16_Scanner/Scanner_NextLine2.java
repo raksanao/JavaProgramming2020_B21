71bb73dca542cb9810da4034749f2b6a3414da57
saimCybertek <saim@cybertekschool.com>
2020-10-15 09:40:27 -0500

    Day 15 Scanner

---
+++ src/day16_Scanner/Scanner_NextLine2.java
@@ -0,0 +1,16 @@
+package day16_Scanner;
+
+import java.util.Scanner;
+
+public class Scanner_NextLine2 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your age");
+
+
+
+
+    }
+}
