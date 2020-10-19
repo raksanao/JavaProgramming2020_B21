4a7233953daac5c880f6bc74a2555d6f28443ca4
cybertekschool <github@cybertekschool.com>
2020-10-19 13:05:29 -0400

    Day18 Class

--- src/day18_Strings/SubStringMethod.java
+++ src/day18_Strings/SubStringMethod.java
@@ -1,5 +1,7 @@
 package day18_Strings;
 
+import java.util.Scanner;
+
 public class SubStringMethod {
 
     public static void main(String[] args) {
@@ -51,8 +53,27 @@ public class SubStringMethod {
 
             output:
                 Cybertek School
-
      */
+
+        Scanner  scan = new Scanner(System.in);
+        System.out.println("Enter your first name: ");
+        String f = scan.nextLine();
+
+        f = f.substring(0,1).toUpperCase()  +  f.substring(1, f.length()-1 +1 ).toLowerCase() ;
+
+        System.out.println("Enter your last name: ");
+        String l = scan.nextLine();
+
+        l =  l.substring(0, 1).toUpperCase() + l.substring(1, l.length()).toLowerCase();
+
+        System.out.println("first name is: "+f);
+        System.out.println("last name is: "+l);
+
+        String fullName = f + " " + l;
+
+        System.out.println("full name is: "+fullName);
+
+
 
 
 
