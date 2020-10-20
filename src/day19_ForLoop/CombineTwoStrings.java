0882c8808c775c3f7f76e0b915638efe9af5f18b
cybertekschool <github@cybertekschool.com>
2020-10-20 10:57:19 -0400

    Day19 Class

--- src/day19_ForLoop/CombineTwoStrings.java
+++ src/day19_ForLoop/CombineTwoStrings.java
@@ -1,11 +1,16 @@
 package day19_ForLoop;
 
+import java.util.Scanner;
+
 public class CombineTwoStrings {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
-        String s1 = "one";
-        String s2 = "eight";
+        String s1 = scan.next();
+        String s2 = scan.next();
+
+        scan.close();
 
         if( s1.charAt( s1.length()-1 ) == s2.charAt(0)  ){ // if last character of first string equal to first character of second string
             System.out.println(s1 + s2.substring(1));
@@ -20,6 +25,8 @@ public class CombineTwoStrings {
         }else{
             System.out.println(s1+s2);
         }
+
+
 
 
     }
