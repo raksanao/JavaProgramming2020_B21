cf395bdff037d31c351dfede2e872d8e6f9517f2
cybertekschool <github@cybertekschool.com>
2020-10-21 15:34:07 -0400

    Office Hour 10/21/2020

---
+++ src/Office_Hours/Practice_10_21_2020/ScannerPractice3.java
@@ -0,0 +1,16 @@
+package Office_Hours.Practice_10_21_2020;
+
+public class ScannerPractice3 {
+
+
+}
+
+/*
+1. ask gender ( next() )
+2. ask age ( nextInt() )
+3. ask full name ( nextLine() )
+4. ask zip code ( nextInt() )
+5. ask country name ( nextLine() )
+6. ask salary ( nextDouble() )
+7. ask company name  ( nextLine() )
+ */
