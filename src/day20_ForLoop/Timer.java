6140ac2a8b8c7c50529ccf075d0d6a5d742243f2
cybertekschool <github@cybertekschool.com>
2020-10-22 12:19:33 -0400

    day20 class

--- src/day20_ForLoop/Timer.java
+++ src/day20_ForLoop/Timer.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Timer {
     public static void main(String[] args) throws InterruptedException {
+
         Scanner scan=new Scanner(System.in);
         System.out.println("please enter the number of minutes");
         int minutes=scan.nextInt();
