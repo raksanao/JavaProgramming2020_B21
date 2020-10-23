ba15393ed864ea6ca3f6280022c93e5744426cfb
cybertekschool <github@cybertekschool.com>
2020-10-23 11:16:34 -0400

    day20 class

--- src/day20_ForLoop/Timer.java
+++ src/day20_ForLoop/Timer.java
@@ -13,7 +13,7 @@ public class Timer {
                 break;
             }
             for (int z=59; z >=0 ; --z) {
-                System.out.println((i-1)+" minutes and "+z+" seconds left");
+                System.out.print("\r"+ (i-1)+" minutes and "+z+" seconds left");
                 Thread.sleep(1000);
             }
         }
