cd5aed94c2c6db212c298fad7af8e25d0d2a349c
cybertekschool <github@cybertekschool.com>
2020-10-26 14:55:27 -0400

    day22 class

--- src/day22_JavaRecap/StringPractice2.java
+++ src/day22_JavaRecap/StringPractice2.java
@@ -11,6 +11,7 @@ public class StringPractice2 {
 
         System.out.println("Enter your lastName: ");
         String l = scan.nextLine();
+        scan.close();
         
         String initial = f.substring(0,1).toUpperCase()+ "." + l.substring(0,1).toUpperCase();
 
