aa5aaa9db7fad91e9d757fba57be4348a2d4cdae
cybertekschool <github@cybertekschool.com>
2020-10-26 11:10:32 -0400

    day22 class

--- src/day22_JavaRecap/MinimumNumber.java
+++ src/day22_JavaRecap/MinimumNumber.java
@@ -17,7 +17,18 @@ public class MinimumNumber {
          */
 
         Scanner scan = new Scanner(System.in);
-        int min
+        int min = 2147483647; // 40
+            // user is most likely to enter a number that's smaller than 2147483647
+
+        for(int i =1 ; i <= 5; i++){
+            System.out.println("Enter a number");
+            int n = scan.nextInt();  // 100, 200, 50, 300, 40
+            if(n < min){
+              min = n;
+            }
+        }
+
+        System.out.println("min = " + min);
 
 
     }
