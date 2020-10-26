132d3ae3bc4cfff0dda2d9ebfcab69b9d581fa8c
cybertekschool <github@cybertekschool.com>
2020-10-26 10:59:34 -0400

    day22 class

--- src/day22_JavaRecap/MinimumNumber.java
+++ src/day22_JavaRecap/MinimumNumber.java
@@ -1,10 +1,23 @@
 package day22_JavaRecap;
 
+import java.util.Scanner;
+
 public class MinimumNumber {
 
     public static void main(String[] args) {
+        /*
+        Scanner object: 1
+        min: 1
 
+        "Enter a num": 5
+        scan.nextInt(): 5
+        comparing:  5
 
+        "Minimum number is: ..": 1
+         */
+
+        Scanner scan = new Scanner(System.in);
+        int min
 
 
     }
