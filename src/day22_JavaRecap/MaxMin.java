74fe211d414a8edb2230899d192fb1313666dd9c
cybertekschool <github@cybertekschool.com>
2020-10-26 11:13:26 -0400

    day22 class

---
+++ src/day22_JavaRecap/MaxMin.java
@@ -0,0 +1,15 @@
+package day22_JavaRecap;
+
+import java.util.Scanner;
+
+public class MaxMin {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int min = 2147483647;
+        int max = -2147483648;
+
+
+    }
+
+}
