c59a6bb1a7348c0b5dc49bf12e1eb413e095f59f
cybertekschool <github@cybertekschool.com>
2020-10-26 11:40:04 -0400

    day22 class

--- src/day22_JavaRecap/MaxMin.java
+++ src/day22_JavaRecap/MaxMin.java
@@ -9,6 +9,23 @@ public class MaxMin {
         int min = 2147483647;
         int max = -2147483648;
 
+        for(int i = 1; i <= 5 ; i++){
+            System.out.println("Enter a number");
+            int n = scan.nextInt();
+
+            if( n < min){ // if the input is smaller than previous min  number
+                min = n;
+            }
+            if(n > max ){  // if the input is greater than previous max number
+               max = n ;
+            }
+
+        }
+
+
+        System.out.println("min = " + min);
+        System.out.println("max = " + max);
+
 
     }
 
