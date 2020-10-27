884ae4244f13ac9ebe072387f93868fcec8b652e
cybertekschool <github@cybertekschool.com>
2020-10-27 13:04:19 -0400

    day23 class

--- src/day23_NestedLoop/NestedLoopPractice.java
+++ src/day23_NestedLoop/NestedLoopPractice.java
@@ -4,10 +4,11 @@ public class NestedLoopPractice {
 
     public static void main(String[] args) {
 
-        for(int j = 1; j <= 8; j++) { // j: 1, 2, 3, 4, 5, 6, 7, 8
+        for(int j = 1; j <= 15; j++) { // j: 1, 2, 3, 4, 5, 6, 7, 8
             System.out.print("\t\t\t\t");
+
             for (int i = 1; i <= j; i++) { // i: 1, 2, 3, 4, 5, 6, 7, 8
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
 
