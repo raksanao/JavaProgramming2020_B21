d580288c2fe22e656899da29a98978426f026d05
cybertekschool <github@cybertekschool.com>
2020-10-30 15:08:11 -0400

    Day25 Class

--- src/day25_ArrayIntro/RoomReservation.java
+++ src/day25_ArrayIntro/RoomReservation.java
@@ -8,19 +8,36 @@ public class RoomReservation {
         Scanner scan = new Scanner(System.in);
         int total = 0;
 
-        System.out.println("Which room would you like to select?");
-        System.out.println("\t\tKing Bed == $120");
-        System.out.println("\t\tQueen Bed == $100");
-        System.out.println("\t\tSingle Bed == 80$");
 
-        String word = scan.nextLine().toLowerCase();
+     while(true) {
+         System.out.println("Which room would you like to select?");
+         System.out.println("\t\tKing Bed == $120");
+         System.out.println("\t\tQueen Bed == $100");
+         System.out.println("\t\tSingle Bed == 80$");
 
-        while( !(word.equals("king bed")||word.equals("queen bed")||word.equals("single bed"))){
-            System.out.println("Invalid Entry, Please Re-Enter");
-            word = scan.nextLine().toLowerCase();
-        } // to make usre user selected either the king bed, or queen bed or single bed
+         String word = scan.nextLine().toLowerCase();
 
+         while (!(word.equals("king bed") || word.equals("queen bed") || word.equals("single bed"))) {
+             System.out.println("Invalid Entry, Please Re-Enter");
+             word = scan.nextLine().toLowerCase();
+         } // to make usre user selected either the king bed, or queen bed or single bed
 
+         total += (word.equals("king bed")) ? 120 : (word.equals("queen bed")) ? 100 : 80;
+
+         System.out.println("Would you like to select another room?");
+         String a = scan.nextLine().toLowerCase();
+
+         while( !( a.equals("yes") || a.equals("no") ) ){ // while the answer is not valid answer
+             System.out.println("Invalid Entry, please re-enter");
+             System.out.println("Would you like to select another room?");
+             a = scan.nextLine().toLowerCase();
+         }
+
+         if(a.equals("no")){
+             break;
+         }
+
+     }
 
 
 
