d6ab68fae5fd1332221caaa2dc516bee55d18e17
cybertekschool <github@cybertekschool.com>
2020-10-30 14:45:13 -0400

    Day25 Class

--- src/day25_ArrayIntro/Calculator.java
+++ src/day25_ArrayIntro/Calculator.java
@@ -7,46 +7,48 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-    while(true) {
-        System.out.println("Enter your first number: ");
-        double n1 = scan.nextDouble();
-        System.out.println("Math Operator: ");
-        char o = scan.next().charAt(0);
-        System.out.println("Enter your second number: ");
-        double n2 = scan.nextDouble();
+        while (true) {
+            System.out.println("Enter your first number: ");
+            double n1 = scan.nextDouble();
+            System.out.println("Math Operator: ");
+            char o = scan.next().charAt(0);
+            System.out.println("Enter your second number: ");
+            double n2 = scan.nextDouble();
 
-        switch (o) {
-            case '+':
-                System.out.println("Addition: " + (n1 + n2));
-                break;
-            case '-':
-                System.out.println("Subtraction: " + (n1 - n2));
-                break;
-            case '*':
-                System.out.println("Multiplication: " + (n1 * n2));
-                break;
-            case '/':
-                System.out.println("Division: " + (n1 / n2));
-                break;
-            default:
-                System.out.println("Invalid");
-        }  // calculates the result
+            switch (o) {
+                case '+':
+                    System.out.println("Addition: " + (n1 + n2));
+                    break;
+                case '-':
+                    System.out.println("Subtraction: " + (n1 - n2));
+                    break;
+                case '*':
+                    System.out.println("Multiplication: " + (n1 * n2));
+                    break;
+                case '/':
+                    System.out.println("Division: " + (n1 / n2));
+                    break;
+                default:
+                    System.out.println("Invalid");
+            }  // calculates the result
 
-        System.out.println("Would you like to continue?");
-        String a = scan.next().toLowerCase();
-
-        while ( !(a.equals("yes") || a.equals("no"))  ){ // while the answer is not valid
-            System.out.println("Invalid Answer, Please Re-Enter");
+            scan.nextLine();
             System.out.println("Would you like to continue?");
-            a = scan.next().toLowerCase();
+            String a = scan.nextLine().toLowerCase();
+
+            while (!(a.equals("yes") || a.equals("no"))) { // while the answer is not valid
+                System.out.println("Invalid Answer, Please Re-Enter");
+                System.out.println("Would you like to continue?");
+                a = scan.nextLine().toLowerCase();
+            }
+
+            if (a.equals("no")) {
+                break;
+            }
+
         }
 
-        if(a.equals("no")) {
-            break;
-        }
-
-    }
-
+        scan.close();
 
 
     }
