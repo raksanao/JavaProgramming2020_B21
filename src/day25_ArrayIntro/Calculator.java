c5196c1ee9b77e83888b212dc84b4aea6b28e112
cybertekschool <github@cybertekschool.com>
2020-10-30 14:25:09 -0400

    Day25 Class

---
+++ src/day25_ArrayIntro/Calculator.java
@@ -0,0 +1,42 @@
+package day25_ArrayIntro;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    while(true) {
+        System.out.println("Enter your first number: ");
+        double n1 = scan.nextDouble();
+        System.out.println("Math Operator: ");
+        char o = scan.next().charAt(0);
+        System.out.println("Enter your second number: ");
+        double n2 = scan.nextDouble();
+
+        switch (o) {
+            case '+':
+                System.out.println("Addition: " + (n1 + n2));
+                break;
+            case '-':
+                System.out.println("Subtraction: " + (n1 - n2));
+                break;
+            case '*':
+                System.out.println("Multiplication: " + (n1 * n2));
+                break;
+            case '/':
+                System.out.println("Division: " + (n1 / n2));
+                break;
+            default:
+                System.out.println("Invalid");
+        }
+
+
+    }
+
+
+
+    }
+
+}
