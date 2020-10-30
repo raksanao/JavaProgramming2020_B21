fdf7b340bb123b0184710a1de40645f02a732961
cybertekschool <github@cybertekschool.com>
2020-10-30 11:07:06 -0400

    Day25 Class

---
+++ src/day25_ArrayIntro/MyFriends.java
@@ -0,0 +1,21 @@
+package day25_ArrayIntro;
+
+public class MyFriends {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
+
+/*
+ how many friends you have?  5,4,3 ...
+
+String[] firstName = new String[5];
+String[] lastName = new String[5];
+
+
+fullName
+ */
