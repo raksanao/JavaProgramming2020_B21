c4b4234bf1c4e4c7545599f57c875b43661c8f83
cybertekschool <github@cybertekschool.com>
2020-11-02 11:01:59 -0500

    Day26 Class

--- src/day26_Arrays/FrequencyOfEachChars2.java
+++ src/day26_Arrays/FrequencyOfEachChars2.java
@@ -4,9 +4,11 @@ public class FrequencyOfEachChars2 {
 
     public static void main(String[] args) {
         String str = "aaabbc";
+           //     str = str.replace(" ",""); // if we need to ignore the spaces
+
         String expectedResult = ""; // "a3"
 
-        for(int j = 0; j <= str.length()-1; j++){
+        for(int j = 0; j <= str.length()-1; j++){  // j represents the index num of str
 
             char ch = str.charAt(j); //a, a, a, b, b, c
             int count = 0;
