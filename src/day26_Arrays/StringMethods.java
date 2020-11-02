49f46b89f8ab76e8f678f8ff7db6039c84742532
cybertekschool <github@cybertekschool.com>
2020-11-02 15:58:52 -0500

    Day26 Class

---
+++ src/day26_Arrays/StringMethods.java
@@ -0,0 +1,36 @@
+package day26_Arrays;
+
+import java.util.Arrays;
+
+public class StringMethods {
+
+    public static void main(String[] args) {
+
+        String str = "abcde";
+
+        char[] array = str.toCharArray();
+
+        System.out.println(  Arrays.toString(array)  );
+
+        System.out.println("==================================");
+        // anagram task:
+        String s1 = "acb";  // a, c, b
+        String s2 = "def"; // b, a, c
+
+        char[] ch1 =  s1.toCharArray(); //[a, c, b]
+        char[] ch2 = s2.toCharArray(); //[b, a, c]
+
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+
+        System.out.println(  Arrays.toString(ch1) );
+        System.out.println( Arrays.toString(ch2) );
+
+        boolean isAnagram = Arrays.equals(ch1, ch2);
+
+        System.out.println(isAnagram);
+
+    }
+
+
+}
