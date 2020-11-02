fc8f181c2efff0ba519dc4f1e90734472c79a8d5
cybertekschool <github@cybertekschool.com>
2020-11-02 16:51:17 -0500

    Day26 Class

---
+++ src/day26_Arrays/FrequencyOfWord.java
@@ -0,0 +1,33 @@
+package day26_Arrays;
+
+public class FrequencyOfWord {
+
+    public static void main(String[] args) {
+
+        String sentence = "I love Java, java is a cool programming language and java is more useful than javascript, python python";
+
+        String[] arr = sentence.split(" ");
+
+        int countJava = 0;
+        int countPython = 0;
+
+        for(int i =0; i <= arr.length-1;  i++){
+            String eachWord = arr[i];
+            if(eachWord.toLowerCase().contains("java")){
+                countJava++;
+            }
+
+            if(eachWord.toLowerCase().contains("python")){
+                countPython++;
+            }
+
+        }
+
+        System.out.println("Java: "+countJava);
+        System.out.println("Python: "+countPython);
+
+
+
+    }
+
+}
