163cfc39f96ae24cd24ed3955195a330c7dd17a8
cybertekschool <github@cybertekschool.com>
2020-11-02 16:36:24 -0500

    Day26 Class

---
+++ src/day26_Arrays/ReverseSentence.java
@@ -0,0 +1,29 @@
+package day26_Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReverseSentence {
+
+    public static void main(String[] args) {
+      //  Scanner scan = new Scanner(System.in);
+       String sentence = "Today is Monday";
+
+       String[] words  = sentence.split(" ");
+
+        System.out.println( Arrays.toString(words) );
+
+        String reversedSentence = "";
+        for(int i = words.length-1; i >=0 ; i--){
+           reversedSentence+= words[i] +" ";
+        }
+
+        reversedSentence = reversedSentence.trim();
+        System.out.println(reversedSentence);
+       // Monday is Today
+
+
+    }
+
+
+}
