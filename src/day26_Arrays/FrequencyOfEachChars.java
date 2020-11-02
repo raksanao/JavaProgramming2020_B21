a43952eb4e1b5a974f9f4e5685e1c2d9c9159cac
cybertekschool <github@cybertekschool.com>
2020-11-02 10:42:25 -0500

    Day26 Class

--- src/day26_Arrays/FrequencyOfEachChars.java
+++ src/day26_Arrays/FrequencyOfEachChars.java
@@ -14,20 +14,20 @@ public class FrequencyOfEachChars {
             }
         }
 
-
         String result = "";  // "a3b2c1"
   // Step 2: find the frequency of each nonDiup characters from the original string
-        for(int j =0; j <= nonDup.length()-1; j++){
+        for(int j =0; j <= nonDup.length()-1; j++){ // j is the index num of nonDup
 
             char ch = nonDup.charAt(j); // a, b, c
             int count = 0;  // to contain the frequency of char ch
 
-            for(int i=0; i <= str.length()-1; i++){
+            for(int i=0; i <= str.length()-1; i++){ // i is the index num of original string
                 char eachChar =  str.charAt(i);  // a, a, a, b, b, c
                 if(ch == eachChar ){
                     count++;
                 }
             }
+
             result +=  ""+ch + count;
 
         }
