6484142011b5bf71634da41145fdfa114628d708
cybertekschool <github@cybertekschool.com>
2020-11-04 15:28:29 -0500

    Office Hour: 11/04/2020

---
+++ src/Office_Hours/Practice_11_04_2020/UniqueChars.java
@@ -0,0 +1,13 @@
+package Office_Hours.Practice_11_04_2020;
+
+public class UniqueChars {
+
+    public static void main(String[] args) {
+
+        String str = "aabccd";
+                           // "bd";
+
+
+    }
+
+}
