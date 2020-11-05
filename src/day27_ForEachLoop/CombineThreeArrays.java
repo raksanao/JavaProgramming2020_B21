5b62f89fcc6c215a3c2332f487eb466e2c090a9f
cybertekschool <github@cybertekschool.com>
2020-11-05 13:05:21 -0500

    Day27 Class

---
+++ src/day27_ForEachLoop/CombineThreeArrays.java
@@ -0,0 +1,41 @@
+package day27_ForEachLoop;
+
+import java.util.Arrays;
+
+public class CombineThreeArrays {
+
+    public static void main(String[] args) {
+
+        String[] group1 = {"Sara", "Jesicca","Parker", "Dragisa", "Sladjan"};
+        String[] group2 = {"James", "Bonda", "Indiana", "Jones","Max", "Payne"};
+        String[] group3 = {"Abdul", "Sacha", "Amin", "Salih", "Jessica"};
+        String[] group4 = {"Justyna", "Adil","Ercan","Irina","Med", "Olesea","Vlad","Asuman","Guvenche"};
+
+
+        String[] groups = new String[group1.length + group2.length+ group3.length +group4.length];
+        int i = 0;
+
+        for(String each : group1){
+            groups[i++] = each;
+        }
+
+        for(String each : group2){
+            groups[i++] = each;
+        }
+
+        for(String each : group3){
+            groups[i++] = each;
+        }
+
+        for(String each : group4){
+            groups[i++] = each;
+        }
+
+
+        System.out.println(Arrays.toString(groups) );
+
+
+
+    }
+
+}
