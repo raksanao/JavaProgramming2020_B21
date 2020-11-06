4fa87393f8d89b21d7465d726edb21a84b5d5e43
cybertekschool <github@cybertekschool.com>
2020-11-06 11:52:25 -0500

    Day28 Class

---
+++ src/day28_MultiDimensionalArray/MultiDArray2.java
@@ -0,0 +1,18 @@
+package day28_MultiDimensionalArray;
+
+public class MultiDArray2 {
+
+    public static void main(String[] args) {
+        int[][]  arr2D1 = {{1,2,3},{4,5,6,7,8}};
+        int[][]  arr2D2 = {{10,20,30},{40,50,60,70,80}};
+        int[][]  arr2D3 = {{100,200,300},{400,500,600,700,800}};
+
+        int[][][]  arr3D = { arr2D1, arr2D2, arr2D3  };
+                       //     0        1        2
+
+
+
+
+    }
+
+}
