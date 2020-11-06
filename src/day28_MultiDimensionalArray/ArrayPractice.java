24be86270d4e2cabe87d0b90070308b7cd78afd8
cybertekschool <github@cybertekschool.com>
2020-11-06 11:27:19 -0500

    Day28 Class

---
+++ src/day28_MultiDimensionalArray/ArrayPractice.java
@@ -0,0 +1,21 @@
+package day28_MultiDimensionalArray;
+
+public class ArrayPractice {
+        /*
+        ScrumTeam
+            Testers:  {"Dorin", "Adil", "Fatih", "Subhi"}
+            Developers: {"Kastrinsi", "Mustafa", "Ruslan"}
+            SM: {"Ayse"}
+            PO: {"Omid"}
+            BA: {"Agalar"}
+
+
+         */
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
