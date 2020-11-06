98275fb948ec0ef57208321b9f281b84a2c9e4da
cybertekschool <github@cybertekschool.com>
2020-11-06 15:04:52 -0500

    Day28 Class

---
+++ src/day28_MultiDimensionalArray/ArrayPractice3.java
@@ -0,0 +1,37 @@
+package day28_MultiDimensionalArray;
+
+public class ArrayPractice3 {
+
+    public static void main(String[] args) {
+        double[] salary = {100000.5,  110000.5, 120000.5, 95000.5, 90000.5};
+        double sum = 0;
+
+        double max = salary[0];
+        double min = salary[0];
+
+        for(double  each : salary ){
+            sum += each;
+
+            if(each > max){
+                max = each;
+            }
+
+            if(each < min){
+                min = each;
+            }
+
+        }
+
+        System.out.println("Total: $"+sum);
+        System.out.println("Average Salary of employees: " +  (sum/salary.length)  );
+        System.out.println("max = " + max);
+        System.out.println("min = " + min);
+
+
+
+
+
+
+    }
+
+}
