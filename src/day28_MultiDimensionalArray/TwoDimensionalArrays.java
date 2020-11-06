cd9c7012ccc10f763658dccca6bc6b3dd8643b87
cybertekschool <github@cybertekschool.com>
2020-11-06 16:29:48 -0500

    Day28 Class

--- src/day28_MultiDimensionalArray/TwoDimensionalArrays.java
+++ src/day28_MultiDimensionalArray/TwoDimensionalArrays.java
@@ -21,6 +21,7 @@ public class TwoDimensionalArrays {
         for(int i = 0; i <= arr2D.length-1; i++  ){ //i: index numbers of single dimensional array
 
             int[] arr1D = arr2D[i];
+
             for(int j =0; j <= arr1D.length-1; j++ ){ // j: index numbers of the elements in each single dimensional array
                 int eachElement = arr1D[j];
 
