bbf5d0f14d9e1b1f74c91f23f3954a4b693a22a2
cybertekschool <github@cybertekschool.com>
2020-11-06 16:30:56 -0500

    Day28 Class

--- src/day28_MultiDimensionalArray/TwoDimensionalArrays2.java
+++ src/day28_MultiDimensionalArray/TwoDimensionalArrays2.java
@@ -4,7 +4,7 @@ public class TwoDimensionalArrays2 {
 
     public static void main(String[] args) {
 
-        int[][] arr2D = {  {1,2,3,4}, {5,600} , {7,8,9}, {10,11,12},  {13,14,15, 16, 17}  };
+        int[][] arr2D = { {1,2,3,4}, {5,600} , {7,8,9}, {10,11,12},  {13,14,15, 16, 17}  };
         //                     0         1          2        3                4
         int max= arr2D[0][0];
         int min = arr2D[0][0];
