e5795148f99e42e6feb181b77588ecfa86766b15
cybertekschool <github@cybertekschool.com>
2020-11-11 16:10:35 -0500

    Office Hour: 11/11/2020

---
+++ src/Office_Hours/Practice_11_11_2020/ClassNote.java
@@ -0,0 +1,60 @@
+package Office_Hours.Practice_11_11_2020;
+
+public class ClassNote {
+
+    /*
+    11/11/2020
+Practice Topics:
+				Arrays & loops
+
+Package name: Practice_11_11_2020
+
+
+Arrays: size is fixed, ordered
+
+	DataType[] arrayName = {Data1, Data2 .... };
+
+	1. Single dimensional array: used to store data
+			[]
+		 int[] age = {28, 30, 25, 27};
+
+
+
+	2. Multi-dimensional array: used to store arrays
+
+						n dimensional array: (n-1) dimensional array
+			[][]
+			[][][]
+			[][][][]
+			....
+
+	3. Arrays utility:
+
+				Arrays.toString(array)
+				Arrays.sort(array)
+				Arrays.equal(array1, array2)
+
+
+for each:
+		there MUST be a data structure
+		order is foxed: from 0 till last index
+		no access to the index numbers
+
+		for( DataType  each  : Collection of data){
+
+		}
+
+
+for loop:
+
+		for(start; end; iterator){
+
+		}
+
+
+
+
+
+     */
+
+}
