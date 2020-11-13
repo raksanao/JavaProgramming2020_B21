e1b206ee2f02d6436ad77888afcd493de9a2eeaf
cybertekschool <github@cybertekschool.com>
2020-11-13 10:35:35 -0500

    Day29 Class

---
+++ src/day29_Methods/Practice.java
@@ -0,0 +1,41 @@
+package day29_Methods;
+
+public class Practice {
+
+    public static void main(String[] args) {
+
+        for(int i = 1; i < 6; i++){
+
+            System.out.println("Hello");
+
+        }
+
+        System.out.println("Muhtar");
+
+        for (int i = 1; i < 6; i++){
+
+            System.out.println("Hello");
+
+        }
+
+        System.out.println("Cybertek");
+
+        for(int i = 1; i < 6; i++){
+
+            System.out.println("Hello");
+
+        }
+
+
+
+    }
+
+}
+
+/*
+	    step1: print hello 5 times  (for loop)
+		step2: print your name
+		step3: print hello 5 times  (for loop)
+		step4: print your school name
+		step5: print hello 5 times   (for loop)
+ */
--- src/day29_Methods/test.java
+++
@@ -1,4 +0,0 @@
-package day29_Methods;
-
-public class test {
-}
