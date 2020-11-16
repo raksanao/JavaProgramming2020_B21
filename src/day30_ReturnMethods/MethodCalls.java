e07f579b8f8cf8532f861d4afde682892938961d
cybertekschool <github@cybertekschool.com>
2020-11-16 16:15:19 -0500

    Day30 Class

--- src/day30_ReturnMethods/MethodCalls.java
+++ src/day30_ReturnMethods/MethodCalls.java
@@ -1,6 +1,9 @@
 package day30_ReturnMethods;
 
 import library.StringUtility;
+import library.ArraysUtility;
+
+import java.util.Arrays;
 
 public class MethodCalls {
 
@@ -20,6 +23,18 @@ public class MethodCalls {
 
         String result3 = StringUtility.unique(str3);
         System.out.println(result3);
+
+        System.out.println("==========================================");
+
+        int[] array = {1, 0, -2, 30, 40, 500, -20, -50};
+
+        int[] descending = ArraysUtility.sort(array);
+
+        System.out.println(Arrays.toString(descending)  );
+        System.out.println( ArraysUtility.printArray(descending)  );
+
+
+
 
     }
 
--- src/library/ArraysUtility.java
+++ src/library/ArraysUtility.java
@@ -50,6 +50,12 @@ public class ArraysUtility {
         }
 
         return reversedArray;
+    }   // sorts in descending order
+
+    public static String printArray(int[] arr){
+        String str = Arrays.toString(arr);
+        str = str.replace("[", "{").replace("]", "}");
+        return str;
     }
 
 
