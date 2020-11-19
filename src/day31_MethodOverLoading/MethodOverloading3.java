5010a15a56a56f0581998c835c52dd1ffb8790bc
cybertekschool <github@cybertekschool.com>
2020-11-19 11:58:55 -0500

    Day31 Class

---
+++ src/day31_MethodOverLoading/MethodOverloading3.java
@@ -0,0 +1,32 @@
+package day31_MethodOverLoading;
+
+public class MethodOverloading3 {
+
+    public static int max(int[] arr){
+        int max =arr[0];
+        for( int each  : arr ){
+            if(each > max){
+                max = each;
+            }
+        }
+        return max;
+    }
+
+    public static double max(double[] arr){
+        double max =arr[0];
+        for( double each  : arr ){
+            if(each > max){
+                max = each;
+            }
+        }
+        return max;
+    }
+
+
+
+
+
+
+
+
+}
