e767551c02b2ba6eaf42cf3b84b981c6f964c1e7
cybertekschool <github@cybertekschool.com>
2020-11-19 10:33:35 -0500

    Day31 Class

--- src/day31_MethodOverLoading/FrequencyOfWord.java
+++ src/day31_MethodOverLoading/FrequencyOfWord.java
@@ -1,30 +1,37 @@
 package day31_MethodOverLoading;
 
+import library.StringUtility;
+
 public class FrequencyOfWord {
 
     public static void main(String[] args) {
         String str = "Today is a great day, today we are learning java, today we have java class today today";
 
-        int count = frequencyOfWord(str, "today");
+        int count = StringUtility.frequencyOfWord(str, "today");
 
         System.out.println(count);
 
+        String str2 = "java JAVA jAvA c#, C#, c# java java";
+        int countJava = StringUtility.frequencyOfWord(str2, "java");
+        int countCSharp = StringUtility.frequencyOfWord(str2, "c#");
+
+        System.out.println("countJava = " + countJava);
+        System.out.println("countCSharp = " + countCSharp);
+
+        System.out.println("==============================================================");
+
+        String sentence = "dog dog dog Dog DOG doG cat cat cat CAT";
+        int countDog = StringUtility.frequencyOfWord(sentence, "dog");
+        int countCat = StringUtility.frequencyOfWord(sentence, "cat");
+
+        System.out.println("countDog = " + countDog);
+        System.out.println("countCat = " + countCat);
+
+        System.out.println( countCat == countDog );
+
+
     }
 
-
-    public static int frequencyOfWord(String sentence, String word){
-        sentence = sentence.toLowerCase();
-        word = word.toLowerCase();
-
-        int frequency = 0;
-
-        while(sentence.contains(word)){
-            sentence = sentence.replaceFirst(word, "");
-            frequency++;
-        }
-
-        return frequency;
-    }
 
 
 }
