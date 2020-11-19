cfde155fdfb9b52880c205cdf1d5a42e9e3a7677
cybertekschool <github@cybertekschool.com>
2020-11-19 10:23:22 -0500

    Day31 Class

--- src/library/StringUtility.java
+++ src/library/StringUtility.java
@@ -118,4 +118,25 @@ public class StringUtility {
     }
 
 
+
+    /*
+    takes a sentence and word, then returns the frequency of the word
+     */
+    public static int frequencyOfWord(String sentence, String word){
+        sentence = sentence.toLowerCase();
+        word = word.toLowerCase();
+
+        int frequency = 0;
+
+        while(sentence.contains(word)){
+            sentence = sentence.replaceFirst(word, "");
+            frequency++;
+        }
+
+        return frequency;
+    }
+
+
+
+
 }
