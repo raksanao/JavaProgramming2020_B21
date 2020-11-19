e59a36e8d8d068e25003dbbdd82db2761b7e5cf7
cybertekschool <github@cybertekschool.com>
2020-11-19 12:38:23 -0500

    Day31 Class

--- src/day31_MethodOverLoading/MethodOverloading4.java
+++ src/day31_MethodOverLoading/MethodOverloading4.java
@@ -1,8 +1,33 @@
 package day31_MethodOverLoading;
 
+import library.ArraysUtility;
+
 import java.util.Arrays;
 
 public class MethodOverloading4 {
+
+    public static void main(String[] args) {
+        int[] arr = {1,4,3,2,7, 0, 10, 11, 9, 8};
+
+          arr = sort(arr);
+
+        System.out.println( Arrays.toString(arr) );
+
+        double[] arr2 = {5.5, 6.5, 10.5, 11.5, 12.5, 3.5, 4.5};
+        arr2 = sort(arr2);
+
+        System.out.println( Arrays.toString(arr2));
+
+
+        char[] arr3 = {'a', 'd', 'b', 'c', 'f'};
+        arr3 = sort(arr3);
+
+        System.out.println(Arrays.toString(arr3));
+
+
+
+    }
+
 
     public static int[] sort(int[] arr){  // {2,1,3}
         Arrays.sort(arr);  // {1,2,3};
@@ -15,7 +40,38 @@ public class MethodOverloading4 {
         return reversedArray;
     }
 
+    public static double[] sort(double[] arr){  // {2,1,3}
+        Arrays.sort(arr);  // {1,2,3};
+        double[] reversedArray = new double[arr.length];  // {3, 2, 1}
 
+        for(int i=arr.length-1, j = 0 ;  i>=0; i--, j++){
+            reversedArray[j] = arr[i];
+        }
+
+        return reversedArray;
+    }
+
+    public static char[] sort(char[] arr){  // {2,1,3}
+        Arrays.sort(arr);  // {1,2,3};
+        char[] reversedArray = new char[arr.length];  // {3, 2, 1}
+
+        for(int i=arr.length-1, j = 0 ;  i>=0; i--, j++){
+            reversedArray[j] = arr[i];
+        }
+
+        return reversedArray;
+    }
+
+    public static String[] sort(String[] arr){
+        Arrays.sort(arr);  // {1,2,3};
+        String[] reversedArray = new String[arr.length];  // {3, 2, 1}
+
+        for(int i=arr.length-1, j = 0 ;  i>=0; i--, j++){
+            reversedArray[j] = arr[i];
+        }
+
+        return reversedArray;
+    }
 
 
 }
