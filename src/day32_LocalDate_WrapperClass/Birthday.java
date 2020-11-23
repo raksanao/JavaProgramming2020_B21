ce448fbbc52b109034579f7dbd0ea1298ecc403b
cybertekschool <github@cybertekschool.com>
2020-11-23 11:28:59 -0500

    Day32 Class

--- src/day32_LocalDate_WrapperClass/Birthday.java
+++ src/day32_LocalDate_WrapperClass/Birthday.java
@@ -4,22 +4,13 @@ import java.time.LocalDate;
 
 public class Birthday {
 
+    public static void main(String[] args) {
 
-    public static void HappyBirthDay(LocalDate DofB){
-        LocalDate today = LocalDate.now();
-        if(today.isEqual(DofB)){
-            System.out.println("Today is your birthday!!!");
-            System.out.println("   \n \t \t  \t \t    * * * * * * * * * * * * * * * * * * * * *");
-            System.out.println("   \t \t  \t \t  *  * * * * * * * * * * * * * * * * * * * *  * ");
-            System.out.println("  \t \t  \t \t *  *                                       *  * ");
-            System.out.println(" \t \t  \t \t*  *          Happy BirthDay To You!         *  * ");
-            System.out.println("  \t \t  \t \t *  *                                       *  * ");
-            System.out.println("  \t \t   \t \t  *  * * * * * * * * * * * * * * * * * * * *  * ");
-            System.out.println("   \t \t   \t \t    * * * * * * * * * * * * * * * * * * * * *");
-            return;
-        }
-        System.out.println("Today is not your birthday");
+
+        HappyBirthDay(11,23);
+
     }
+
 
     public static void HappyBirthDay(int month, int day){
         LocalDate birthday = LocalDate.of(1, month, day);
