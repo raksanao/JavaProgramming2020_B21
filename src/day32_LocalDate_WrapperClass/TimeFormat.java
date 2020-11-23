d40faea734c4061b122e8b11a4e382b370925a44
cybertekschool <github@cybertekschool.com>
2020-11-23 13:08:56 -0500

    Day32 Class

--- src/day32_LocalDate_WrapperClass/TimeFormat.java
+++ src/day32_LocalDate_WrapperClass/TimeFormat.java
@@ -23,7 +23,7 @@ LocalTime:
 
  */
     public static void main(String[] args) {
-        DateTimeFormatter df = DateTimeFormatter.ofPattern("MMMM/dd/yy EEEE");
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("MMMM/dd/yyyy, EEEE");
 
         LocalDate today = LocalDate.now();  //2020-11-23
         System.out.println(today);
@@ -47,10 +47,61 @@ LocalTime:
         // Monday, 12:40 PM,  Nov/23/2020
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("EEE, hh:mm a, MMMM/dd/yyyy");
 
-        LocalDateTime dt1 = LocalDateTime.of(2020, 11, 23, 12, 40);
+        LocalDateTime dt1 = LocalDateTime.now();
 
         System.out.println(dt1);
         System.out.println(dt1.format(dtf));
+
+
+        // task:
+
+        LocalDate tomorrow =  LocalDate.of(2020, 11, 24);
+        System.out.println(tomorrow.format(df));
+
+        boolean r1 = today.isBefore(tomorrow); // true
+        boolean r2 = today.isAfter(tomorrow);  // false
+
+        System.out.println(r1);
+        System.out.println(r2);
+
+        // calculate the age
+                        // 2020-11-23      2020
+        int currentYear = LocalDate.now().getYear();
+        LocalDate DofB = LocalDate.of(2000, 5, 19);
+
+        int age = currentYear - DofB.getYear();
+
+        System.out.println(age);
+
+        System.out.println(DofB.plusDays(10));
+
+        LocalDate rightNow = LocalDate.now();
+
+        LocalDate holidayBreak = rightNow.plusDays(2);
+
+        System.out.println(holidayBreak);
+
+
+        LocalTime time = LocalTime.now();
+        LocalTime lunchBreak = time.plusMinutes(15);
+
+        System.out.println(lunchBreak.format(tf));
+
+        System.out.println("===============================================");
+
+        LocalTime classStarts = LocalTime.of(10, 0);
+        LocalTime firstBreak = classStarts.plusMinutes(45);
+        System.out.println(firstBreak);
+
+        LocalTime secondBreak = firstBreak.plusHours(1);
+        System.out.println(secondBreak);
+
+        LocalTime thirdBreak = secondBreak.plusHours(1).plusMinutes(15);
+        System.out.println(thirdBreak);
+
+        LocalTime afternoonClass = thirdBreak.plusHours(1).plusMinutes(15);
+        System.out.println("Come back at: "+afternoonClass.format(tf));
+
 
 
     }
