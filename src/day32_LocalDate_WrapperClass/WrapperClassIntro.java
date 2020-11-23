aa1379bf7172df098b366d343e0edfcb71e09a43
cybertekschool <github@cybertekschool.com>
2020-11-23 15:20:01 -0500

    Day32 Class

---
+++ src/day32_LocalDate_WrapperClass/WrapperClassIntro.java
@@ -0,0 +1,24 @@
+package day32_LocalDate_WrapperClass;
+
+public class WrapperClassIntro {
+
+    public static void main(String[] args) {
+        byte b = 100;
+        Byte b2 = b;
+       // Integer b3 = (int)b;
+
+        int i = 10;
+      //  Byte b3 = i;  // wrapper class is only dedicated to its own primitive
+
+        Integer I = 20;
+        double d = I;
+
+        Integer num = 100;
+    //    Long L1 = num;
+        long L2 = num;
+
+
+
+    }
+
+}
