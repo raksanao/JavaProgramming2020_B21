a3fc5d5e7e66b7b8592f4c33d0cd1912513731fc
cybertekschool <github@cybertekschool.com>
2020-11-24 11:21:19 -0500

    Day33 Class

--- src/day33_ArrayList/WrapperClassPractice.java
+++ src/day33_ArrayList/WrapperClassPractice.java
@@ -6,14 +6,45 @@ public class WrapperClassPractice {
         String str = "a1b2c3d4";  // 1: 49,  2: 50, 3: 51
         int sum = 0 ;
 
-        for(char each : str.toCharArray() ){
-            if(Character.isDigit(each)){
-                sum += Integer.parseInt( ""+each );  // each digits need to be converted to integer and added to the sum
+        for(int i = 0; i <= str.length()-1; i++ ){
+            if(Character.isDigit(str.charAt(i))){  // to verify if each char in string is a digit
+                sum += Integer.parseInt( ""+str.charAt(i) );  // each digits need to be converted to integer and added to the sum
             }
         }
 
 
         System.out.println("sum = " + sum);
+
+        System.out.println("====================================================");
+            String password = "Word#7";
+
+            String upperLetters = "";
+            String lowerLetters = "";
+            String digits = "";
+            String specialChar = "";
+
+            for(char each : password.toCharArray() ){
+                if(Character.isUpperCase(each) ){ // verify if the character is upper case
+                    upperLetters += each;
+                }else if(Character.isLowerCase(each)){ // verify if the character is lower case
+                    lowerLetters += each;
+                }else if(Character.isDigit(each)){ // verify if the character is digit
+                    digits += each;
+                }else{ //' special character
+                    specialChar += each;
+                }
+            }
+
+        System.out.println("upperLetters = " + upperLetters);
+        System.out.println("lowerLetters = " + lowerLetters);
+        System.out.println("digits = " + digits);
+        System.out.println("specialChar = " + specialChar);
+
+        boolean isValid = password.length() >= 8 && upperLetters.length()>0 && lowerLetters.length() >0
+                          && digits.length() > 0 && specialChar.length()>0;
+
+        System.out.println(isValid);
+
 
 
     }
@@ -30,4 +61,13 @@ public class WrapperClassPractice {
                 input: "Today's date is 04/27/2020"
                 output: 17
                     (0+4+2+7+2+0+2+0=17)
+
+
+Password validation
+    1. 8 character at least
+    2. there must be a digit            (isUpper   isLowercase)
+    3. there must be a letter (at least one upper case & one lower case)
+    4. there must be special character
+
+
  */
