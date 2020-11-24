bb8f40d9ed1bc97d0f3d151287f0e8d2ee57ddbc
cybertekschool <github@cybertekschool.com>
2020-11-24 10:49:11 -0500

    Day33 Class

--- src/day33_ArrayList/DateTimePractice.java
+++ src/day33_ArrayList/DateTimePractice.java
@@ -10,27 +10,66 @@ public class DateTimePractice {
     public static void main(String[] args) {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("E, hh:mm a, MMMM/dd/yyyy");
 
-        LocalDateTime d1 = LocalDateTime.of(2020,11,24, 13,0);
+        LocalDateTime d1 = LocalDateTime.of(2020, 11, 24, 13, 0);
 
-        System.out.println( d1.format(dtf).replaceFirst("PM", "pm") );
+        System.out.println(d1.format(dtf).replaceFirst("PM", "pm"));
 
         System.out.println("=====================================================");
         DateTimeFormatter df = DateTimeFormatter.ofPattern("MMM-dd, E");
-        LocalDate[]  days  = new LocalDate[5];
+        LocalDate[] days = new LocalDate[5];
 
-        for (int i = 0; i <= days.length-1; i++){
-            days[i] = LocalDate.now().plusDays(i+1);
+        for (int i = 0; i <= days.length - 1; i++) {
+            days[i] = LocalDate.now().plusDays(i + 1);
         }
 
         System.out.println(Arrays.toString(days));
 
 
-        for(LocalDate each  : days ){
+        for (LocalDate each : days) {
             System.out.println(each.format(df));
         }
 
 
         System.out.println("=====================================================");
+        String[] friends = {"Polina", "Serhii", "Aziz", "Halzat", "Abdujilil", "Ayse"};
+        LocalDate[] dOfB = {
+                LocalDate.of(1991, 9, 25),
+                LocalDate.of(1990, 11, 23),
+                LocalDate.of(1995, 02, 10),
+                LocalDate.of(2000, 03, 15),
+                LocalDate.of(2005, 04, 20),
+                LocalDate.of(2010, 05, 25)
+        };
+
+        String nameOfOlder = friends[0];
+        LocalDate older = dOfB[0];
+
+        String nameOfYoungest = friends[0];
+        LocalDate younger = dOfB[0];
+
+        for(int i =0; i <= dOfB.length-1; i++){
+
+            if(dOfB[i].isBefore(older) ){
+                older = dOfB[i];
+                nameOfOlder = friends[i];
+            }
+
+            if( dOfB[i].isAfter(younger)){
+                younger = dOfB[i];
+                nameOfYoungest = friends[i];
+            }
+
+
+        }
+
+
+        System.out.println(nameOfOlder+" : "+older);
+        System.out.println(nameOfYoungest+" : "+younger);
+
+
+
+
+
 
 
 
