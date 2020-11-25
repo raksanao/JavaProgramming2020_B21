0c7dca22b7dbae469743f9fdedf28cb2a9cecbcd
cybertekschool <github@cybertekschool.com>
2020-11-25 15:06:37 -0500

    Day33 Class

--- src/Office_Hours/Practice_11_25_2020/MethodPractice.java
+++ src/Office_Hours/Practice_11_25_2020/MethodPractice.java
@@ -9,43 +9,87 @@ public class MethodPractice {
         divisibleBy3Between1To100();
         System.out.println("How are you");
 
-        divisibleBy3(50, 50);
+        divisibleBy3(100, 10);
 
-        System.out.println("Test");
+
+    //    grade(5.5);
+        grade(85);
+
+
+
 
     }
 
-    public static void divisibleBy3Between1To100(){
+    public static void divisibleBy3Between1To100() {
 
-        for(int i =1; i<=100; i++){
-            if(i%3 != 0){  // if the number is not divisible by3
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 != 0) {  // if the number is not divisible by3
                 continue; // then skip
             }
-            System.out.print(i+" ");
+            System.out.print(i + " ");
         }
 
         System.out.println();
     }
 
-    public static void divisibleBy3(int start, int end){
-        if(end <= start){
+    public static void divisibleBy3(int start, int end) {
+
+        if (end <= start) {
             System.out.println("Invalid Numbers");
-           // System.exit(0);  //
+            // System.exit(0);  //
             return;  // exits the current method
         }
 
-        for(int i = start ; i<=end; i++){
-            if(i%3 != 0){  // if the number is not divisible by3
+        for (int i = start; i <= end; i++) {
+            if (i % 3 != 0) {  // if the number is not divisible by3
                 continue; // then skip
             }
-            System.out.print(i+" ");
+            System.out.print(i + " ");
         }
 
         System.out.println();
 
     }
 
+    /*
+    A: Excellent
+    B: Great
+    C: Good
+    D: Passed
+    F: Failed
+     */
 
+    public static void grade(int score) {
+
+        if (score < 0 || score > 100) {  // if score is invalid
+            System.out.println("Invalid Score");
+            return; // exits the current method ONLY
+        }
+
+        String grade = (score >= 90) ? "A" : (score >= 80) ? "B" : (score >= 70) ? "C" : (score >= 60) ? "D" : "F";
+
+        switch (grade) {
+            case "A":
+                System.out.println("Excellent, You made an A");
+                break;
+
+            case "B":
+                System.out.println("Great, You made a B");
+                break;
+
+            case "C":
+                System.out.println("Good, You made a C");
+                break;
+
+            case "D":
+                System.out.println("You passed");
+                break;
+
+            default:
+                System.out.println("You Failed");
+        }
+
+    }
 
 
 }
