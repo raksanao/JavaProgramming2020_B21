bfbe0be25f35b26edd45be046673dbf6b2cf7346
cybertekschool <github@cybertekschool.com>
2020-11-27 16:47:49 -0500

    Office Hour: 11/27/2020

--- src/Office_Hours/Practice_11_27_2020/MathPractice.java
+++ src/Office_Hours/Practice_11_27_2020/MathPractice.java
@@ -1,5 +1,7 @@
 package Office_Hours.Practice_11_27_2020;
 
+import java.text.DecimalFormat;
+
 public class MathPractice {
 
     public static void main(String[] args) {
@@ -33,8 +35,27 @@ public class MathPractice {
         System.out.println("max = " + max);
 
 
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+
+        double n5 = Math.sqrt(100); // square root of 100
+        System.out.println(n5);
+
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        double r1 = calculateAreaOfCircle(3);
+        System.out.println(r1);
+        System.out.println(  df.format(r1)    );
+
+        System.out.println( df.format(2.9877777));
 
 
     }
+
+    public static double calculateAreaOfCircle(double r){
+        return Math.PI * Math.pow(r,2);
+    }
+
+
 
 }
