bd1e71c7894e7622498354cfbc70ec68154096fe
cybertekschool <github@cybertekschool.com>
2020-11-27 15:51:17 -0500

    Office Hour: 11/27/2020

---
+++ src/Office_Hours/Practice_11_27_2020/CharacterPractice.java
@@ -0,0 +1,32 @@
+package Office_Hours.Practice_11_27_2020;
+
+public class CharacterPractice {
+
+    public static void main(String[] args) {
+
+        int countSpecialChars = 0;
+        int countLetters = 0;
+        int countDigits = 0;
+
+
+        for(int i = 1;  i<= Character.MAX_VALUE; i++){
+            char each = (char)i;
+            if(!Character.isLetterOrDigit(each)) {
+                // Character.isLetter(each) for printing all letters
+               countSpecialChars++;
+            }else if(Character.isLetter(each)){
+                countLetters++;
+            }else{
+                countDigits++;
+            }
+        }
+
+        System.out.println("countSpecialChars = " + countSpecialChars);
+        System.out.println("countLetters = " + countLetters);
+        System.out.println("countDigits = " + countDigits);
+
+
+
+    }
+
+}
