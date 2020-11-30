e9de4d8c4382d833feb34229a140b80b516d5f52
cybertekschool <github@cybertekschool.com>
2020-11-30 14:38:31 -0500

    Day34 Class

--- src/day34_ArrayList/FrequencyOfCharacters.java
+++ src/day34_ArrayList/FrequencyOfCharacters.java
@@ -1,6 +1,7 @@
 package day34_ArrayList;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class FrequencyOfCharacters {
@@ -8,12 +9,12 @@ public class FrequencyOfCharacters {
         String str = "AAABBCDDDDEEE";
         String result = "";             // A3B2C1D4E3
 
-        ArrayList<String> list = new ArrayList<>(); // in order to be able to use frequency method
-
+        ArrayList<String> list = new ArrayList<>( Arrays.asList( str.split(""))  ); // in order to be able to use frequency method
+/*
         for(String each : str.split("")) {  // we get every character from str and add them into arraylist so that we can use frequency method
             list.add(each);
         }
-
+*/
 
         for(String each : list ){
             int frequency = Collections.frequency(list, each);
