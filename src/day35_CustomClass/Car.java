23955c38bb1fd4858f5e05b4e6040dbb5ad3d8c1
cybertekschool <github@cybertekschool.com>
2020-12-01 12:46:58 -0500

    Day35 Class

---
+++ src/day35_CustomClass/Car.java
@@ -0,0 +1,21 @@
+package day35_CustomClass;
+
+public class Car {
+
+    public String brand;  // instance variables
+    public String model;
+
+    public void setInfo(String carBrand, String carModel){ // Instance methods
+        brand = carBrand;
+        model =carModel;
+    }
+
+}
+/*
+Car class:
+		Attributes: instance variables
+			brand, model, color, price, year....
+
+		actions: instance methods
+			driver(), stop()....
+ */
