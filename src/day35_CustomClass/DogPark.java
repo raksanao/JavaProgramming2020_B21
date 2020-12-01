9b3629bf41c0a5a8bdef9c9bfd98992a12d89e74
cybertekschool <github@cybertekschool.com>
2020-12-01 16:05:16 -0500

    Day35 Class

---
+++ src/day35_CustomClass/DogPark.java
@@ -0,0 +1,58 @@
+package day35_CustomClass;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class DogPark {
+
+    public static void main(String[] args) {
+
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog();
+        Dog dog3 = new Dog();
+        Dog dog4 = new Dog();
+        Dog dog5 = new Dog();
+        Dog dog6 = new Dog();
+
+        dog1.setInfo("Tima", "York", "small", 'M',
+                "multicolor", LocalDate.of(2010,01,19));
+
+        dog1.getInfo();
+
+        dog2.setInfo("Balli","Maltese", "small", 'F',
+                "white", LocalDate.of(2019,12,01));
+
+        dog2.getInfo();
+
+        dog3.setInfo("Richard", "Chow Chow", "Medium", 'M',
+                "Brown", LocalDate.of(2015,7,2));
+
+        dog3.getInfo();
+
+        dog1.eat("Chicken");
+
+        dog2.drink("Beer");
+
+
+        Dog[] dogs = {dog1, dog2, dog3};
+
+        ArrayList<Dog> huskies = new ArrayList<>();
+        ArrayList<Dog> Maltese = new ArrayList<>();
+
+        for(Dog eachDog: dogs){
+            if( eachDog.breed.equalsIgnoreCase("husky") ){
+                huskies.add(eachDog);
+            }else if( eachDog.breed.equalsIgnoreCase("maltese") ){
+                Maltese.add(eachDog);
+            }
+        }
+
+        System.out.println("Total Number of Huskies: "+ huskies.size());
+        System.out.println("Total Number of Maltese: "+ Maltese.size());
+
+
+
+
+    }
+
+}
