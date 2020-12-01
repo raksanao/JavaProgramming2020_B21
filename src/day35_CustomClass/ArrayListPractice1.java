c9da74de329b71efaa4c17b7e8b7f972cd574316
cybertekschool <github@cybertekschool.com>
2020-12-01 10:16:15 -0500

    Day35 Class

---
+++ src/day35_CustomClass/ArrayListPractice1.java
@@ -0,0 +1,31 @@
+package day35_CustomClass;
+
+public class ArrayListPractice1 {
+
+    public static void main(String[] args) {
+        String[] countryNames = {"Andorra","United Arab Emirates","Afghanistan","Turkey","United States"," Antigua and Barbuda","Albania", "Colombia", "Venezuela", "Panama","United Kingdom", "Mexico ", "United States"};
+
+
+    }
+
+
+}
+/*
+task01:
+			1. create an Array of string called country names
+			2. write a program that can remove all the country names that have length of 10 or greater
+
+	task02:
+			1. create an Array of LocalDate
+			2. write a program that can remove all the dates before August-15-2016
+
+	task03:
+			1. create an Array of String called jobTitles
+			2. write a program that only keeps the jobTitles that are: SDET and QA
+
+	task04:
+		Given a list of Integers 1, 2, 3, 4, 5, 6 ....etc. remove all values greater than 100.
+
+				solution 1: use lambda
+				solution 2: do not use lambda
+ */
