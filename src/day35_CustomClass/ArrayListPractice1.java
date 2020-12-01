856a76bad5a31ca732d5cba324f91e628600a8e8
cybertekschool <github@cybertekschool.com>
2020-12-01 10:59:29 -0500

    Day35 Class

--- src/day35_CustomClass/ArrayListPractice1.java
+++ src/day35_CustomClass/ArrayListPractice1.java
@@ -60,13 +60,4 @@ task01:
 			1. create an Array of LocalDate
 			2. write a program that can remove all the dates before August-15-2016
 
-	task03:
-			1. create an Array of String called jobTitles
-			2. write a program that only keeps the jobTitles that are: SDET and QA
-
-	task04:
-		Given a list of Integers 1, 2, 3, 4, 5, 6 ....etc. remove all values greater than 100.
-
-				solution 1: use lambda
-				solution 2: do not use lambda
  */
---
+++ src/day35_CustomClass/ArrayListPractice2.java
@@ -0,0 +1,47 @@
+package day35_CustomClass;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayListPractice2 {
+
+    public static void main(String[] args) {
+
+        String[] jobTitles = {"SDET", "Scrum Master", "QA", "Developer", "PO", "BA", "CEO", "SDET", "QA", "Developer", "SDET"};
+
+        ArrayList<String> jobs = new ArrayList<>(  );
+        jobs.addAll( Arrays.asList(jobTitles)  );
+
+        jobs.retainAll(    Arrays.asList( "SDET", "QA" )  );
+
+        jobs.removeIf( p ->  !p.equals("SDET") && !p.equals("QA")   );
+
+        System.out.println(jobs);
+
+        System.out.println("===========================================================");
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+            numbers.addAll( Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,-1, -2, -3, -4, -1,0,1,2,3));
+
+            // only keep the numbers 7, 8, 9
+            numbers.retainAll(  Arrays.asList(7, 8, 9)  );
+
+
+        System.out.println(numbers);
+
+
+
+    }
+
+}
+/*
+task03:
+			1. create an Array of String called jobTitles
+			2. write a program that only keeps the jobTitles that are: SDET and QA
+
+	task04:
+		Given a list of Integers 1, 2, 3, 4, 5, 6 ....etc. remove all values greater than 100.
+
+				solution 1: use lambda
+				solution 2: do not use lambda
+ */
