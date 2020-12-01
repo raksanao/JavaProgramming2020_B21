ed8dd796dd604a0457103b79790bfaac1bf04638
cybertekschool <github@cybertekschool.com>
2020-12-01 15:33:27 -0500

    Day35 Class

--- src/day35_CustomClass/Dog.java
+++ src/day35_CustomClass/Dog.java
@@ -1,4 +1,24 @@
 package day35_CustomClass;
 
+import java.time.LocalDate;
+
 public class Dog {
+
+    public String nickName;
+    public String breed;
+    public String size;
+    public char gender;
+    public String color;
+    public LocalDate DOB;
+
+    public void setInfo(String dogNickName, String dogBreed, String dogSize, char dogGender, String dogColor, LocalDate dogDOB){
+        nickName = dogNickName;
+        breed = dogBreed;
+        size = dogSize;
+        gender = dogGender;
+        color = dogColor;
+        DOB = dogDOB;
+    }
+
+
 }
