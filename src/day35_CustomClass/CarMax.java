9ace32835b039eb274cc142d01953e824e10eae1
cybertekschool <github@cybertekschool.com>
2020-12-01 14:57:46 -0500

    Day35 Class

--- src/day35_CustomClass/CarMax.java
+++ src/day35_CustomClass/CarMax.java
@@ -1,5 +1,8 @@
 package day35_CustomClass;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CarMax {
 
     public static void main(String[] args) {
@@ -12,7 +15,7 @@ public class CarMax {
         car1.setInfo("Toyota", "Camry", "White", 2017, 10000);
       //  car1.getInfo();
 
-        car2.setInfo("Honda","CR-V","Black", 2019, 15000.55 );
+        car2.setInfo("Honda","CR-V","Black", 2015, 15000.55 );
        // car2.getInfo();
 
         car3.setInfo("Nissan", "R32 GTR", "Dark Red", 1991, 55000.99);
@@ -20,6 +23,25 @@ public class CarMax {
         car4.setInfo("Range Rover"," HSE Hybrid","White",2019,98000);
 
         car5.setInfo("Audi","R8","White",2020,105000);
+
+        Car[]  cars = { car1, car2, car3, car4, car5};
+
+        for( Car eachCar : cars){
+            eachCar.getInfo();
+        }
+
+        System.out.println("============================================");
+
+        ArrayList<Car> carList = new ArrayList<>();
+        carList.addAll(  Arrays.asList(cars)  );
+
+        carList.removeIf( p -> p.price < 25000 ); // we removed all the cars that are under 25k
+
+        for(int i = 0; i <= carList.size()-1; i++){
+            carList.get(i).getInfo();
+        }
+
+
 
 
     }
