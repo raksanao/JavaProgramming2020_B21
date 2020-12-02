2a6cd1bb7db694b05800b5ecfa5bef6b26c3d516
cybertekschool <github@cybertekschool.com>
2020-12-02 16:03:16 -0500

    Office Hour: 12/02/2020

---
+++ src/Office_Hours/Practice_12_02_2020/ClassNotes.java
@@ -0,0 +1,43 @@
+package Office_Hours.Practice_12_02_2020;
+
+public class ClassNotes {
+    /*
+    12/02/2020
+Topics: ArrayList methods:
+				Bulk Operations
+				removeIf
+
+		Collections Utility
+
+package name: Practice_12_02_2020
+
+3 Data Structure:
+		1. Array
+		2. Collection  ==> Iterable
+		3. Map
+
+ArrayList methods:
+		get
+		set
+		contains
+		clear
+		equals
+
+		addAll(CollectionType)
+		removeAll(CollectionType)
+		retainAll(CollectionType)
+
+		removeIf
+
+
+Collections utility:
+		sort
+		swap
+		frequency
+		max
+		min
+
+
+
+     */
+}
