193fb745ebe3d8e6848654efacaf1900a4d5adec
cybertekschool <github@cybertekschool.com>
2020-12-03 11:33:06 -0500

    day36 class

--- src/day36_CustomClass/BankOfAmerica.java
+++ src/day36_CustomClass/BankOfAmerica.java
@@ -15,8 +15,17 @@ public class BankOfAmerica { // Bank of America HAS An Employee
                 new Employee()
         };
 
-        employees[0].setInfo("Angelina Jolie", 'F', "Developer", companyName, 400_000, "0001", LocalDate.of(2018, 04, 15));
+        employees[0].setInfo("Angelina Jolie", 'F', "Developer", companyName, 400_000, "0001", LocalDate.of(2018, 4, 15));
+        employees[1].setInfo("Brad Pitt",'M',"Scrum Master",companyName,140_000,"0003",LocalDate.of(2016, 9,14));
+        employees[2].setInfo("Obi-Wan Kenobi", 'M', "SDET", companyName, 164000, "3-CPO", LocalDate.of(2010, 2, 14));
+        employees[3].setInfo("Ercan Civi", 'M', "SDET", companyName, 164000.50, "ABC123", LocalDate.of(2000, 2, 14));
+        employees[4].setInfo("John Snow",'M',"SDET",companyName,240_000,"0004",LocalDate.of(2006, 2,18));
+
+
         employees[0].getInfo();
+
+
+
 
 
 
