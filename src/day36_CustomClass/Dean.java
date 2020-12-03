f93e8ff812705eeb1fe8813434ee5f97a62c2859
cybertekschool <github@cybertekschool.com>
2020-12-03 12:45:07 -0500

    day36 class

---
+++ src/day36_CustomClass/Dean.java
@@ -0,0 +1,38 @@
+package day36_CustomClass;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Dean { // Dean HAS An Offer
+
+
+    public static void main(String[] args) {
+
+        ArrayList<Offer> offers = new ArrayList<>();
+        offers.addAll(Arrays.asList( new Offer(), new Offer(), new Offer(), new Offer(), new Offer() ));
+
+        offers.get(0).setInfo("CA", "SDET", "CapitalOne", 120000, true, true, false, false);
+        offers.get(1).setInfo("VA", "QA", "Apple", 115000, true, true, true, true);
+        offers.get(2).setInfo( "VA", "Scrum Master", "American Express", 100000, true, false, false, true);
+        offers.get(3).setInfo("NY","SDET","MIT",150000,true,true,false,false);
+        offers.get(4).setInfo("TX", "SDET", "Bank Of America", 100000, true, false, true, true);
+
+        /*
+       for(int i = 0; i <= offers.size()-1; i++){
+          offers.get(i).getInfo();
+       }
+         */
+
+        System.out.println("===========================================================");
+        // only print the offers from VA
+
+        for(Offer each : offers){
+
+        }
+
+
+
+
+    }
+
+}
