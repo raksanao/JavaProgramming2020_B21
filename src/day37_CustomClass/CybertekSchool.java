52462509aacd6fc72cef1712c78c879fdfffbd46
cybertekschool <github@cybertekschool.com>
2020-12-04 14:06:37 -0500

    day37 class

---
+++ src/day37_CustomClass/CybertekSchool.java
@@ -0,0 +1,36 @@
+package day37_CustomClass;
+
+import java.time.LocalDate;
+
+public class CybertekSchool { // Has A
+
+    public static void main(String[] args) {
+
+        Student student1 = new Student();
+        Student student2 = new Student();
+        Student student3 = new Student();
+        Student student4 = new Student();
+        Student student5 = new Student();
+        Student student6 = new Student();
+        Student student7 = new Student();
+        Student student8 = new Student();
+        Student student9 = new Student();
+        Student student10 = new Student();
+
+        student1.setInfo("Aaron", 'M', LocalDate.of(2000,11,2), "A01", 1.5);
+        student2.setInfo("Daniel", 'M', LocalDate.of(1990, 10, 25), "B123", 3.2);
+        student3.setInfo("Vanya", 'M', LocalDate.of(1998, 10, 12), "C324", 3.3);
+        student4.setInfo("Jessica", 'F', LocalDate.of(1999, 8,19), "G345", 5.3);
+        student5.setInfo("Jennifer", 'F', LocalDate.of(1990, 12,13), "F435", 2.3);
+        student6.setInfo("Kostya", 'M', LocalDate.of(2000, 9,7), "J09", 2.5);
+        student7.setInfo("Adel", 'F', LocalDate.of(1999, 1,20), "L092", 6.5);
+        student8.setInfo("Bob", 'M', LocalDate.of(1989, 6,29), "Y45", 1.3);
+        student9.setInfo("John", 'M', LocalDate.of(2001, 7,12), "L05", 1.3);
+        student10.setInfo("Sanya", 'M', LocalDate.of(1990, 3,29), "P93", 3.9);
+
+
+
+
+    }
+
+}
