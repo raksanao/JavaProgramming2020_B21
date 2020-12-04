3a43b8efb0463bcf313362e96f3aacb3bec86e30
cybertekschool <github@cybertekschool.com>
2020-12-04 16:22:54 -0500

    day37 class

---
+++ src/day37_CustomClass/Employee.java
@@ -0,0 +1,46 @@
+package day37_CustomClass;
+
+public class Employee {
+
+    public String name, company, jobTitle, ID, gender;
+    public int weeklyHours;
+    public double hourlyRate, salary, totalTax, salaryAfterTax;
+
+    public Employee(String name, String company, String jobTitle, String ID, String gender, int weeklyHours, double hourlyRate) {
+        this.name = name;
+        this.company = company;
+        this.jobTitle = jobTitle;
+        this.ID = ID;
+        this.gender = gender;
+        this.weeklyHours = weeklyHours;
+        this.hourlyRate = hourlyRate;
+        this.salary = calculateSalary();
+    }
+
+    public double calculateSalary(){
+        return hourlyRate * weeklyHours * 52;
+    }
+
+    public double calculateTax(double stateTaxRate, double federalTaxRate){
+        return calculateSalary() * stateTaxRate +  calculateSalary() *federalTaxRate;
+    }
+
+    public double calculateSalaryAfterTax(){
+        return salary - totalTax;
+    }
+
+
+
+
+
+
+}
+
+/*
+    setInfo
+    toString
+
+
+
+
+ */
