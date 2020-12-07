14657e94df46954f93f9df93e3112e81a8bef025
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-07 11:15:32 -0500

    day38 class

--- src/day38_Statics/SalaryCalculatorObjects.java
+++ src/day38_Statics/SalaryCalculatorObjects.java
@@ -4,7 +4,7 @@ public class SalaryCalculatorObjects {
 
     public static void main(String[] args) {
         SalaryCalculator salary1 = new SalaryCalculator();
-        salary1.setInfo(55, 40, 8,22);
+        salary1.setInfo(60, 40, 8,22);
 
         System.out.println(salary1);
 
@@ -12,6 +12,8 @@ public class SalaryCalculatorObjects {
         salary2.setInfo(55, 40, 8,22);
 
         // ==
+
+        System.out.println( salary1.equals(salary2)  );
 
 
     }
