e2ebad4490d9926835329ad542acf6cd5342b50a
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-07 14:32:09 -0500

    day38 class

---
+++ src/day38_Statics/ScrumTask/Tester.java
@@ -0,0 +1,37 @@
+package day38_Statics.ScrumTask;
+
+public class Tester {
+
+    public String name, ID, jobTitle, companyName;
+    public double salary;
+    public char gender;
+
+    public void setInfo(String name, String ID, String jobTitle, String companyName, double salary, char gender) {
+        this.name = name;
+        this.ID = ID;
+        this.jobTitle = jobTitle;
+        this.companyName = companyName;
+        this.salary = salary;
+        this.gender = gender;
+    }
+
+    public void smokeTesting(){
+        System.out.println(name+" is performing smoke testing");
+    }
+
+    public void creatingTicket(){
+        System.out.println(name+" is creating ticket");
+    }
+
+
+
+}
+
+/*
+create a class called Tester
+					Attributes:
+						name, employeeID, JobTitle, Salary
+
+					Actions:
+						setInfo(), smokeTesting(),  creatingTicket(), toString()
+ */
