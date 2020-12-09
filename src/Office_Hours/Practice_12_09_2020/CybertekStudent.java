ef4461d0a28716e9f1c2f57a7409d083349b973f
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-09 14:34:37 -0500

    Office Hour: 12/09/2020

---
+++ src/Office_Hours/Practice_12_09_2020/CybertekStudent.java
@@ -0,0 +1,22 @@
+package Office_Hours.Practice_12_09_2020;
+
+public class CybertekStudent {
+        public String name;
+        public int age;
+        public char gender;
+
+        public static String schoolName = "Cybertek School" , dreamJob = "SDET";
+        public static boolean hasReplIt = true, hasFlipGrid = true;
+
+        public void setInfo(String name, int age, char gender){
+               this.name = name;
+               this.age = age;
+               this.gender = gender;
+        }
+
+        public String toString(){
+                return "Name: "+name+", Age: "+age +", GenderL: "+gender+", School Name: "+schoolName+", dream job"+dreamJob;
+        }
+
+
+}
