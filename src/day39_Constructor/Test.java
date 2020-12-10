dbd34c49d09acb446668bf1b0810f144b8113c5f
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-10 12:46:59 -0500

    day39 class

--- src/day39_Constructor/Test.java
+++ src/day39_Constructor/Test.java
@@ -1,9 +1,17 @@
 package day39_Constructor;
 
+//import library.Data;  // imports everything that are static or non static
+
+import static library.Data.data1;
+import static library.Data.data2;
+
+import static library.Data.*;
+
+import static library.ArraysUtility.*;
+
 public class Test {
 
     public static void main(String[] args) {
-
 
         System.out.println(StaticBlocks.company );
         System.out.println(StaticBlocks.employee1);
@@ -14,6 +22,17 @@ public class Test {
 
         System.out.println(StaticBlocks.company );
 
+        System.out.println("=====================================================");
+
+        System.out.println( data1 );
+        System.out.println( data2 );
+
+        method1();
+        method2();
+
+        int[] arr = {1,2,3,4,5,6};
+        int max = max(arr);
+        int min = min(arr);
 
 
 
