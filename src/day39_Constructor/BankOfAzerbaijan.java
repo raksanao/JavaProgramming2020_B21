2505aba241795a8ab0826475ee7baacc7a8ba6ab
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-10 12:12:53 -0500

    day39 class

---
+++ src/day39_Constructor/BankOfAzerbaijan.java
@@ -0,0 +1,21 @@
+package day39_Constructor;
+
+
+import day37_CustomClass.Employee;
+
+import java.util.ArrayList;
+
+public class BankOfAzerbaijan  {
+
+    public static void main(String[] args) {
+
+        ArrayList<Employee> employees = new ArrayList<>();
+
+        employees.add( HumanResource.employee1 );
+        employees.add( HumanResource.employee2 );
+
+        System.out.println( employees.get(0) );
+
+
+    }
+}
