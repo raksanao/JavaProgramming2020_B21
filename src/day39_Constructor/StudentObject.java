cc9161807a6588ca3c99fd69db3cfeecb644d025
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-10 12:27:02 -0500

    day39 class

---
+++ src/day39_Constructor/StudentObject.java
@@ -0,0 +1,12 @@
+package day39_Constructor;
+
+public class StudentObject {
+
+    public static void main(String[] args) {
+        CybertekStudent student1 = new CybertekStudent();
+
+        System.out.println(student1);
+
+    }
+
+}
