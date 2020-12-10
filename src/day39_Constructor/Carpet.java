43ea4daee6466bd5887ab2fb0e7d9a4a69f2b1bf
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-10 10:12:39 -0500

    day39 class

---
+++ src/day39_Constructor/Carpet.java
@@ -0,0 +1,35 @@
+package day39_Constructor;
+
+public class Carpet {
+
+    public double width, length, unitPrice;
+    public boolean isPersian;
+
+    public void customOrder(double width, double length, double unitPrice, boolean isPersian){
+        this.width = width;
+        this.length = length;
+        this.unitPrice = unitPrice;
+        this.isPersian = isPersian;
+    }
+
+
+
+}
+
+/*
+1. create a custom class for the Carpet class that should contain the following:
+
+                instance variables:
+                        width, length, unitPrice, isPersian (boolean)
+
+                instance methods:
+                        customOrder(): sets the carpet' width, length, unitprice, & isParsian
+
+                        calcCost(): should be able to caculate the total cost of the carpet and return it as double
+
+                        toSrring(): should be able to display all the info of the carpet including the total cost of the carpet as calculated by calcCost()
+
+            total price of carpet= (width*length)*unitprice
+
+            if the carpet is persian  carpet that's came from Turkey, add 200$ to the totalPrice
+ */
