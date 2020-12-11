49e79b355353aa24bd6155ca05009c5b2d5a6fe1
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-11 15:58:47 -0500

    day40 class

---
+++ src/day40_Constructor/Car.java
@@ -0,0 +1,38 @@
+package day40_Constructor;
+
+public class Car {
+    public String brand, model, color;
+    public int year;
+    public double price;
+
+    public Car(String brand) {
+        this.brand = brand;
+    }
+
+
+
+
+}
+
+/*
+2. class name: Car
+			instance variables:
+					brand, model, year, price, color
+
+			1st constructor: initialize the brand of the car
+
+			2nd constructor: initialize the brand and model of the car
+							(MUST apply constructor Call)
+
+			3rd constructor: initialize the brand, model, year of the car
+							(MUST apply constructor Call)
+
+			4th constructor: initialize the brand, model, year, pric of the car
+							(MUST apply constructor Call)
+
+			5th Constructor: initialize all isntances of the car
+							(MUST apply constructor Call)
+
+			Instance methods:
+				toString()
+ */
