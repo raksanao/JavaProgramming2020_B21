09622a88ac3b78c09dd71422c89150ab3a39792c
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-11 12:11:42 -0500

    day39 class

---
+++ src/day40_Constructor/MySalaries.java
@@ -0,0 +1,25 @@
+package day40_Constructor;
+
+public class MySalaries {
+
+    public static void main(String[] args) {
+
+        SalaryCalculator salary1 = new SalaryCalculator(40, 55, 9, 22);
+
+        SalaryCalculator salary2 = new SalaryCalculator(40, 55, 0, 23);
+
+        SalaryCalculator salary3 = new SalaryCalculator(45, 35, 8, 21);
+
+        SalaryCalculator salary4 = new SalaryCalculator(45, 65, 8, 24);
+
+        SalaryCalculator salary5 = new SalaryCalculator(50, 70, 9, 22);
+
+
+        // print the maximum salary after tax
+
+
+
+
+    }
+
+}
