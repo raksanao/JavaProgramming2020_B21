c47346e7740dbccfbdef947e85f4a402eef89cb8
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-11 16:03:32 -0500

    day40 class

--- src/day40_Constructor/Car.java
+++ src/day40_Constructor/Car.java
@@ -9,6 +9,25 @@ public class Car {
         this.brand = brand;
     }
 
+    public Car(String brand, String model) {
+        this(brand);
+        this.model = model;
+    }
+
+    public Car(String brand, String model, int year) {
+       this(brand, model);
+        this.year = year;
+    }
+
+    public Car(String brand, String model, int year, double price) {
+        this(brand, model, year);
+        this.price = price;
+    }
+
+    public Car(String brand, String model, String color, int year, double price) {
+        this(brand, model, year, price);
+        this.color = color;
+    }
 
 
 
