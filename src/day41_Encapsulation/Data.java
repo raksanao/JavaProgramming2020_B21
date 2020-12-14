70d7003f44862cb4741ed0aab3b7b6cb8e693ef9
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-14 11:37:43 -0500

    day41 class

---
+++ src/day41_Encapsulation/Data.java
@@ -0,0 +1,12 @@
+package day41_Encapsulation;
+
+public class Data {
+
+    public static String publicVariable = "A";
+    private static String privateVariable = "B";
+    static String defaultVariable = "C";  // default access modifier
+
+    // protected static String protectedVariable = "D";
+
+
+}
---
+++ src/day41_Encapsulation/Test.java
@@ -0,0 +1,14 @@
+package day41_Encapsulation;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(Data.publicVariable);
+    //    System.out.println(Data.privateVariable);
+        System.out.println(Data.defaultVariable);
+
+
+
+    }
+
+}
