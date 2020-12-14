dbeac3c1ae7075fb1a68b967b10069db1042d49a
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-14 15:38:57 -0500

    day41 class

--- src/day41_Encapsulation/CircleObjects.java
+++ src/day41_Encapsulation/CircleObjects.java
@@ -4,6 +4,20 @@ public class CircleObjects {
 
     public static void main(String[] args) {
 
+        Circle c1 = new Circle(4);
+
+       // System.out.println(c1.area);
+        System.out.println(c1.getDiameter());
+        System.out.println(c1.getArea());
+        System.out.println(c1.getPerimeter() );
+
+        System.out.println("=======================");
+        c1.setRadius(5);
+        System.out.println(c1.getDiameter());
+        System.out.println(c1.getArea());
+        System.out.println(c1.getPerimeter() );
+
+
     }
 
 }
