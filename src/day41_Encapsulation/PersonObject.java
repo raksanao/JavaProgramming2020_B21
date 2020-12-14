38e2532e39d7238248b729c3ee57002383e918b2
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-14 14:59:43 -0500

    day41 class

--- src/day41_Encapsulation/PersonObject.java
+++ src/day41_Encapsulation/PersonObject.java
@@ -1,15 +1,17 @@
 package day41_Encapsulation;
 
+import java.time.LocalDate;
+
 public class PersonObject {
 
     public static void main(String[] args) {
 
-        Person p1 = new Person("Jimmy", 19, 'M');
+        Person p1 = new Person("Jimmy", 19, 'M', LocalDate.of(2001,5,6));
        // System.out.println(p1.name);
         System.out.println( p1.getName() );
 
         p1.setName("Breanna");
-        p1.setGender('F');
+       // p1.setGender('F');
         p1.setAge(23);
 
         System.out.println( p1.getName() );
