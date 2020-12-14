ab54931a8ac147df9516f086be2674aa77ab5c96
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-14 11:43:09 -0500

    day41 class

---
+++ src/library/Test.java
@@ -0,0 +1,18 @@
+package library;
+
+import static day41_Encapsulation.Data.*;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println(publicVariable);
+     //   System.out.println(privateVariable);
+    //    System.out.println(defaultVariable);
+
+
+
+
+    }
+
+}
