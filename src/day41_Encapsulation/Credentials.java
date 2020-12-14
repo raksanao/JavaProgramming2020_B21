1ae377b3b53fc3dd9ad238b0c604f3505cb9ec14
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-14 13:07:08 -0500

    day41 class

---
+++ src/day41_Encapsulation/Credentials.java
@@ -0,0 +1,22 @@
+package day41_Encapsulation;
+
+public class Credentials {
+
+    private String userName = "cybertek";
+    private double passWord = 1234;
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public double getPassWord() {
+        return passWord;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+
+}
