92d0cb107946c364c3be297a68eb6461b04a3489
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-17 11:59:48 -0500

    day42 class

--- src/day42_Inheritance/AnimalTask/Dog.java
+++ src/day42_Inheritance/AnimalTask/Dog.java
@@ -4,8 +4,13 @@ package day42_Inheritance.AnimalTask;
 public class Dog extends Animal {
 
     public Dog(String name, String breed, char gender, int age, String color, String size ){
-        setInfo();
+       setInfo(name, breed, gender, age, color, size);
     }
+
+    public void bark(){
+        System.out.println(name+" is barking");
+    }
+
 
 }
 
