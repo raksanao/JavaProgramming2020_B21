706e19dba36d0c3668969f747145149a2064f129
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-17 11:50:22 -0500

    day42 class

--- src/day42_Inheritance/AnimalTask/Animal.java
+++ src/day42_Inheritance/AnimalTask/Animal.java
@@ -6,6 +6,15 @@ public class Animal {
     public char gender;
     public int age;
 
+    public void setInfo(String name, String breed, char gender, int age, String color, String size) {
+        this.breed = breed;
+        this.color = color;
+        this.name = name;
+        this.size = size;
+        this.gender = gender;
+        this.age = age;
+    }
+
     public void eat(String food){
         System.out.println(name+" is eating "+food);
     }
@@ -17,5 +26,17 @@ public class Animal {
     public void sleep(){
         System.out.println(name+" is sleeping");
     }
+
+    public String toString() {
+        return "Animal{" +
+                "breed='" + breed + '\'' +
+                ", color='" + color + '\'' +
+                ", name='" + name + '\'' +
+                ", size='" + size + '\'' +
+                ", gender=" + gender +
+                ", age=" + age +
+                '}';
+    }
+
 
 }
---
+++ src/day42_Inheritance/AnimalTask/Dog.java
@@ -0,0 +1,22 @@
+package day42_Inheritance.AnimalTask;
+
+
+public class Dog extends Animal {
+
+    public Dog(String name, String breed, char gender, int age, String color, String size ){
+        setInfo();
+    }
+
+}
+
+
+/*
+class C{
+
+    public static void main(String[] args) {
+        new Dog().eat("Food");
+                new Animal().eat("Food");
+    }
+}
+
+ */
