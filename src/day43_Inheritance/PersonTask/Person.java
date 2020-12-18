cca35396f5b2e7507bfae42787e5a7b5700eef3f
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-18 10:44:27 -0500

    day42 class

---
+++ src/day43_Inheritance/PersonTask/Person.java
@@ -0,0 +1,30 @@
+package day43_Inheritance.PersonTask;
+
+public class Person {
+
+    public String name;
+    public int age;
+    public char gender;
+
+    public void setInfo(String name, int age, char gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public void eat(){
+        System.out.println(name+" is eating");
+    }
+
+    public void sleep(){
+        System.out.println(name+" is sleeping");
+    }
+
+
+}
+
+/*
+Person
+        name, age, gender
+        setInfo, eat(), sleep()
+ */
