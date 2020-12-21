4ff3fcb78c928f6739d2ba6a647666b76b19060b
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-21 15:12:52 -0500

    day44 class

---
+++ src/day44_Inheritance/CarTask/Car.java
@@ -0,0 +1,19 @@
+package day44_Inheritance.CarTask;
+
+public class Car {
+
+    public String brand, model, color;
+    public double price;
+    public int year;
+
+    public Car(String brand, String model, String color, double price, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.price = price;
+        this.year = year;
+    }
+
+
+
+}
