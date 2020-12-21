483b518a7059f8953829fcd47b677fda178d7d98
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-21 12:54:24 -0500

    day43 class

--- src/day44_Inheritance/ShapesTask/Triangle.java
+++ src/day44_Inheritance/ShapesTask/Triangle.java
@@ -2,12 +2,22 @@ package day44_Inheritance.ShapesTask;
 
 public class Triangle  extends Shape{
 
-    public double height, base;
+    public double height, base, side;
 
-    public Triangle( double height, double base) {
+    public Triangle( double height, double base, double side) {
         super("Triangle");
         this.height = height;
         this.base = base;
+        this.side = side;
     }
 
+    @Override
+    public double calcArea() {
+        return base * height * 0.5;
+    }
+
+    @Override
+    public double calcPerimeter() {
+        return base + side * 2;
+    }
 }
