a78d06d41d347884d7529be71371d0716fa687f8
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-21 10:44:08 -0500

    day43 class

--- src/day44_Inheritance/ShapesTask/Square.java
+++ src/day44_Inheritance/ShapesTask/Square.java
@@ -1,4 +1,18 @@
 package day44_Inheritance.ShapesTask;
 
-public class Square {
+public class Square extends Shape {
+
+   /*
+    variables: 4 ( 1 ins, 3 sta)
+    methods: 4 (4 ins)
+ */
+    public double side;
+
+    public Square(double side){
+        this.side = side;
+        setInfo("Square");
+    }
+
+
+
 }
