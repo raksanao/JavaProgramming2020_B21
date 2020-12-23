af335e23b59686048410bd748036867e47d06a5a
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-23 14:49:00 -0500

    Office Hour: 12/23/2020

--- src/Office_Hours/Practice_12_23_2020/Tester.java
+++ src/Office_Hours/Practice_12_23_2020/Tester.java
@@ -2,6 +2,11 @@ package Office_Hours.Practice_12_23_2020;
 
 public class Tester extends Employee {
 
+    public final static boolean isCool;
+
+    static{
+        isCool = true;
+    }
 
     public Tester(String name, String jobTitle, String ID, char gender, double salary, boolean isFullTime) {
         super(name, jobTitle, ID, gender, salary, isFullTime);
