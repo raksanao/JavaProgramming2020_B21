62d6902ff12468026e0a6267a6e0351bf4127b47
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-23 14:47:17 -0500

    Office Hour: 12/23/2020

---
+++ src/Office_Hours/Practice_12_23_2020/Tester.java
@@ -0,0 +1,11 @@
+package Office_Hours.Practice_12_23_2020;
+
+public class Tester extends Employee {
+
+
+    public Tester(String name, String jobTitle, String ID, char gender, double salary, boolean isFullTime) {
+        super(name, jobTitle, ID, gender, salary, isFullTime);
+    }
+
+
+}
