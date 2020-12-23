f6695095d0c74a90aed4b70d4c385f41fc4b56d9
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-23 15:28:56 -0500

    Office Hour: 12/23/2020

---
+++ src/Office_Hours/Practice_12_23_2020/ScrumTeam.java
@@ -0,0 +1,63 @@
+package Office_Hours.Practice_12_23_2020;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ScrumTeam {
+
+    private ProductOwner productOwner;  // ScrumTeam Has A Product Owner
+    private ScrumMaster scrumMaster; // ScrumTeam Has A ScrumMaster
+    private BusinessAnalyst businessAnalyst; // ScrumTeam Has A Business Analyst
+
+    public ArrayList<Tester> testers = new ArrayList<>();
+    public ArrayList<Developer> developers = new ArrayList<>();
+
+    public ScrumTeam(ProductOwner productOwner, ScrumMaster scrumMaster, BusinessAnalyst businessAnalyst){
+        setProductOwner(productOwner);
+        setScrumMaster(scrumMaster);
+        setBusinessAnalyst(businessAnalyst);
+    }
+
+    public void addTester(Tester tester){
+        testers.add(tester);
+    }
+
+    public void addDeveloper(Developer developer){
+        developers.add(developer);
+    }
+
+    public void addTester(Tester[] testers){
+       this.testers.addAll( Arrays.asList(testers) );
+
+      //  for(Tester each : testers ) addTester(each);
+    }
+
+
+
+
+    public ProductOwner getProductOwner(){
+        return productOwner;
+    }
+
+    public void setProductOwner(ProductOwner productOwner){
+        this.productOwner = productOwner;
+    }
+
+    public ScrumMaster getScrumMaster() {
+        return scrumMaster;
+    }
+
+    public void setScrumMaster(ScrumMaster scrumMaster) {
+        this.scrumMaster = scrumMaster;
+    }
+
+    public BusinessAnalyst getBusinessAnalyst() {
+        return businessAnalyst;
+    }
+
+    public void setBusinessAnalyst(BusinessAnalyst businessAnalyst) {
+        this.businessAnalyst = businessAnalyst;
+    }
+
+
+}
